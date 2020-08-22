dce51afd596cfc59d6de6d9ee65535b8bdba1d5c
Yogesh Rathod <yrathod33@gmail.com>
2020-08-22 18:04:33 +0530

    minor changes

--- src/Authentication/login/router.js
+++ src/Authentication/login/router.js
@@ -2,7 +2,6 @@ import { login } from './handler'
 
 export default function (app) {
     app.post('/login', function (req, res) {
-        //verify email and password
         login(req, res)
     })
 }
--- src/Authentication/signup/handler.js
+++ src/Authentication/signup/handler.js
@@ -3,6 +3,6 @@ import { processUserData } from './domain'
 async function signup(req, res) {
     const userData = await processUserData(req.body)
     await saveUsers(userData)
-    res.json({ login: 'done' })
+    res.json({ signUp: 'done' })
 }
 export { signup }
