625595f4d0a9ecac3634b44ded4d0884e523171a
Yogesh Rathod <yrathod33@gmail.com>
2020-08-22 18:03:52 +0530

    added migration script for candidate

---
+++ migrations/20200822175204_candidates.ts
@@ -0,0 +1,31 @@
+const Knex = require('knex')
+
+exports.up = function (knex) {
+    const query = `
+
+    CREATE SCHEMA candidates;
+
+    CREATE TABLE candidates.candidates (
+        "createdAt" timestamptz DEFAULT now(),
+        "updatedAt" timestamptz DEFAULT now(),
+        id serial PRIMARY KEY,
+        first_name TEXT,
+        last_name TEXT,
+        email TEXT UNIQUE,
+        web_address TEXT,
+        cover_letter TEXT,
+        attachment TEXT,
+        do_you_like_working boolean
+    );
+        
+    CREATE TRIGGER candidates_updated
+    BEFORE INSERT OR UPDATE
+    ON candidates.candidates
+    FOR EACH ROW
+    EXECUTE PROCEDURE update_updated_at(); 
+    `
+
+    return knex.schema.raw(query)
+}
+
+exports.down = function (knex) {}
